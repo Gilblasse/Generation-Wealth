3c51ff3ea70b5b7c1fa8c52a610f011561155f04
Gilblasse <Gilblasse@gmail.com>
2020-08-04 21:36:05 -0400

    added react routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import SignUpForm from './components/SignUpForm';
+import AdminLogin from './components/AdminLogin';
+import { Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      HELLO
+
+      <Switch>
+        <Route exact path="/signup" component={SignUpForm}/>
+        <Route exact path="/admin" component={AdminLogin}/>
+        <Route exact path="/" component={SignUpForm}/>
+      </Switch>
+      
     </div>
   );
 }
